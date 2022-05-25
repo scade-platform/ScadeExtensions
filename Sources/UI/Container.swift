b1a8159655d0a69621572549948f908710556671
shem <vsv@scade.io>
2022-05-25 14:45:35 +0200

    add subscript with type for widget container

--- Sources/UI/Container.swift
+++ Sources/UI/Container.swift
@@ -12,4 +12,16 @@ public extension SCDWidgetsContainer {
   subscript(name: String) -> SCDWidgetsWidget? {
     self.getWidgetByName(name)
   }
+
+  /// Get child widget by name and type.
+  ///
+  ///    container["label", as: SCDWidgetsLabel.self]?.text = "hello"
+  ///
+  /// - Parameters:
+  ///   - name: The name of child widget.
+  ///   - type: The type of child widget.
+  /// - Returns: A child widget with name `name` and type `type`.
+  subscript<Result: SCDWidgetsWidget>(name: String, as type: Result.Type) -> Result? {
+    self.getWidgetByName(name) as? Result
+  }
 }
